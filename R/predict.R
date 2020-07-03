e27dc50751715c56a80ed40b944e7d460151f016
Nathan Carroll <nathanjcarroll.econ@gmail.com>
2020-07-03 17:44:18 +0200

    start predict function development

---
+++ R/predict.R
@@ -0,0 +1,11 @@
+
+
+predict.oglmx <- function(object, newdata = NULL, type = c("link", "response"),
+                                  outcome = "all", na.action, ...){
+  objectTerms <- terms(object)
+  if (missing(newdata) || is.null(newdata)){
+    
+  } else {
+    
+  }
+}
